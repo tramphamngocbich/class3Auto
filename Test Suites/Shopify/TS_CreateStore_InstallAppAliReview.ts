b5e434560650e6025740e2c317494bcd2d2f14a1
PC <PC@DESKTOP-F18FU36>
2023-08-15 22:44:22 +0700

    commit

--- Test Suites/Shopify/TS_CreateStore_InstallAppAliReview.ts
+++ Test Suites/Shopify/TS_CreateStore_InstallAppAliReview.ts
@@ -1,11 +1,11 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <TestSuiteCollectionEntity>
    <description></description>
-   <name>TS_Login_CreateStore_InstallAppAliReview</name>
+   <name>TS_CreateStore_InstallAppAliReview</name>
    <tag></tag>
    <delayBetweenInstances>0</delayBetweenInstances>
    <executionMode>SEQUENTIAL</executionMode>
-   <maxConcurrentInstances>8</maxConcurrentInstances>
+   <maxConcurrentInstances>7</maxConcurrentInstances>
    <testSuiteRunConfigurations>
       <TestSuiteRunConfiguration>
          <configuration>
